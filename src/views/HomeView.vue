85013e1a628a8e42143e5b4d8ed4bdb08f14a27e
Giorgio Pulli <giorgiopulli@gmail.com>
2026-05-20 11:23:52 +0200

    fix(HomeView): adjust layout and positioning of elements for improved responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="relative min-h-screen overflow-hidden bg-secondary text-primary flex items-center justify-center opacity-100">
-      <div class="absolute h-[90svh] top-[-1px] opacity-100">
+  <div class="relative h-[93.3vh] bg-secondary text-primary flex items-center justify-center opacity-100">
+      <div class="absolute h-[87svh] top-[-1px] opacity-100">
         <img :src="bgGif" class="h-full w-auto max-w-none object-contain object-center">
       </div>
     
 
-    <div class="bottonihome absolute top-[90vh] flex justify-between gap-x-[5px] w-[50vw]">
+    <div class="bottonihome absolute top-[85vh] flex justify-between gap-x-[5px] w-[50vw]">
       <router-link
         v-for="link in homeLinks"
         :key="link.to"
         :to="link.to"
         class="flex items-center justify-center whitespace-nowrap text-primary hover:text-tertiary transition-colors">
-        <div class="rounded-full bg-tertiary flex-shrink-0"></div>
         <div class="text-[2vw] font-bold"> {{ link.label }} </div>
       </router-link>
     </div>
@@ -19,7 +18,6 @@
 </template>
 
 <script setup>
-import logoSvg from '../../assets/logo.svg'
 import bgGif from '../../assets/bg.gif'
 
 const homeLinks = [
